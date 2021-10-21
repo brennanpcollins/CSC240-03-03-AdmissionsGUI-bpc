1a083feac373c7493bb73f56b8c694f8eaf794a0
W. Scott Cordle <scordle@sullivan.edu>
2021-10-21 15:18:40 -0400

    Added a few comments

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// see my comments below -- wsc 10/21/2021
+
 namespace CSC240_03_03_AdmissionsGUI_bpc
 {
     public partial class Form1 : Form
@@ -30,24 +32,22 @@ namespace CSC240_03_03_AdmissionsGUI_bpc
             //PROCESS
             if ((GPA >= 3.00) && (score >= 60))
             {
-                UxResultTextBox3.Text = "Accepted!";
-                UxResultTextBox3.Visible = true;
+                UxResultTextBox3.Text = "Accepted!"; //OUTPUT
+                UxResultTextBox3.Visible = true; // is this necessary? Isn't it always on?
             }
             else
             {
                 if ((GPA < 3.00) && (score >= 80))
                 {
-                    UxResultTextBox3.Text = "Accepted!";
-                    UxResultTextBox3.Visible = true;
+                    UxResultTextBox3.Text = "Accepted!"; //OUTPUT
+                    UxResultTextBox3.Visible = true; // see above
                 }
                 else
                 {
-                    UxResultTextBox3.Text = "Rejected... Sorry!";
-                    UxResultTextBox3.Visible = true;
+                    UxResultTextBox3.Text = "Rejected... Sorry!"; //OUTPUT
+                    UxResultTextBox3.Visible = true; // ditto
                 }
             }
-            //OUTPUT
-
         }
     }
 }
